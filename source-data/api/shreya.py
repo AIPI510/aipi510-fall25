278fa971c96849ea146388abea5dcda6a9923409
Shreya Mendi <shreyamendi@Shreyas-MacBook-Air.local>
2025-09-02 13:08:02 -0400

    dataframe with the jokeapi

--- source-data/api/shreya.py
+++ source-data/api/shreya.py
@@ -8,7 +8,5 @@ response=requests.get("https://v2.jokeapi.dev/joke/Programming?amount=25")
 data=response.json()
 df= pd.json_normalize(data)
 #normalizing the data to create a dataframe
-df["jokes"]=df.apply(lambda row: [row["setup"] + " " + row["delivery"]] if row["type"]=="twopart" else [row["joke"]], axis=1)
-#combining the setup and delivery columns for two-part jokes, and keeping the joke column for single-part jokes
-# df=df.explode("jokes").reset_index(drop=True)
+df=df.explode("jokes").reset_index(drop=True)
 print(df.head(5))
