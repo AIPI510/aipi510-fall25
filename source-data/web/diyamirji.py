710462a68c2f24803175beaba03087e575109e12
dvm14 <diya.mirji@duke.edu>
2025-09-02 13:52:29 -0400

    Diya Mirji's src code for web lab2

--- source-data/web/diyamirji.py
+++ source-data/web/diyamirji.py
@@ -4,7 +4,6 @@ import requests
 from bs4 import BeautifulSoup, Comment
 import pandas as pd
 
-#"https://www.york.ac.uk/teaching/cws/wws/webpage1.html"
 response = requests.get("https://www.basketball-reference.com/leagues/NBA_2024_totals.html") #to download HTML from the selected page
 #data = response.text
 #print(data)
@@ -13,4 +12,9 @@ soup = BeautifulSoup(response.text, 'html.parser')
 table = soup.find("table", {"id": "totals_stats"})
 
 df = pd.read_html(str(table))[0]
-df.head()
+df = df[['Player', 'Age', 'PTS']]
+print(df.head())
+
+# this DataFrame holds the table for the top total points for players
+# the columns of this DataFrame are the player, age, and total points
+# so this data could be used to show if age is relevant to how many points the players will make
