00b2591ea1fc622e182be819178a3a4d06d57b30
sam-packer <me@sampacker.com>
2025-09-02 14:01:54 -0400

    Delete files from week2 branch, only include my Python files

--- source-data/arduino/get_arduino_data.py
+++
@@ -1,8 +0,0 @@
-import serial
-import time
-ser = serial.Serial('/dev/tty.usbmodem1101', 9600)  # Replace with your port, Run in terminal:ls /dev/tty.*
-time.sleep(2)  # Wait for connection
-for i in range(10):  # Read 10 lines
-    line = ser.readline().decode('utf-8').strip()
-    print(line)
-ser.close()
