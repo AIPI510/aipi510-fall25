e75ee2578e044b7cf3c4cd6506d26617c85d70e6
Jonas Neves <jonas.neves@duke.edu>
2025-09-09 09:01:29 -0400

    Fix web scraping code and update comments

--- source-data/web/jonas.py
+++ source-data/web/jonas.py
@@ -5,15 +5,29 @@ import pandas as pd
 response = requests.get("https://arxiv.org/list/cs.AI/recent")
 soup = BeautifulSoup(response.text, 'html.parser')
 
-articles = soup.find_all('div', class_='list-title mathjax')
+data = []
+dts = soup.find_all('dt')[:10]
 
-# first 5 titles with authors
-for article in articles[:5]:
-    title = article.get_text(strip=True)
-    print(f"Title: {title}")
-    authors = article.find_next('div', class_='list-authors').get_text(strip=True)
-    print(f"Authors: {authors}")
+for dt in dts:
+    a_tag = dt.find('a', title='Abstract')
+    if a_tag:
+        arxiv_id = a_tag.text.strip()
+        link = 'https://arxiv.org' + a_tag['href']
+        
+        dd = dt.find_next_sibling('dd')
+        if dd:
+            title_span = dd.find('span', class_='descriptor', string='Title:')
+            title = title_span.next_sibling.strip() if title_span else 'N/A'
+            
+            authors_div = dd.find('div', class_='list-authors')
+            authors = authors_div.get_text(strip=True).replace('Authors: ', '') if authors_div else 'N/A'
+            
+            data.append({'arxiv_id': arxiv_id, 'title': title, 'authors': authors, 'link': link})
 
-# This is getting 5 articles from the cs.AI category on arXiv
-# Could be used for keeping on top of the most recent developments in AI research
-# Potentially useful for  users that want to be on top of recent papers.
+df = pd.DataFrame(data)
+df = df.dropna(subset=['title'])
+print(df[['arxiv_id', 'title']].head(5))
+
+# Scrapes recent cs.AI papers from arXiv: arXiv ID, title, authors, link.
+# Cleans incomplete rows via dropna.
+# I'd use it to curate AI paper summaries, e.g., scan titles/authors for LLM trends.
