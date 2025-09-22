f3252fe6211d23e48687663b60646ce8c866e523
duke-aipi-Han <han.wang2@duke.edu>
2025-09-21 22:23:37 -0400

    remove extra shell script and update notebook to pull data file directly.

--- data-exploration/get_data.sh
+++
@@ -1,4 +0,0 @@
-# script to load data into data/wee4 folder
-# need to have a clean environment, and should only be run once.
-
-git subtree add --prefix data/week4 https://github.com/Welltory/hrv-covid19.git master --squash
